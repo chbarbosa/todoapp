c11d42f548a0ead1e29ac3b26004ab560dc177c8
CH Barbosa <carlos.barbosa@serpro.gov.br>
2019-05-13 13:51:37 -0300

    todo schema added

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,2 +1,4 @@
 const mongoose = require('mongoose');
+mongoose.Primise = global.Promise;
+module.exports = mongoose.connect('mongodb://localhost/todo');
 
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -6,9 +6,9 @@ const express = require('express');
 
 const server = express();
 
-server.use(bodyParser.urlencoded({extended:true}));
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
-server.listen(port, function() {
+server.listen(port, function () {
     console.log(`BACKEND is running on port ${port}.`)
 });
