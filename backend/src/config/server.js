0b916acaea8d3b88af84fdb03a28300243e41dbf
CH Barbosa <carlos.barbosa@serpro.gov.br>
2019-05-13 13:54:24 -0300

    es6 hint fix

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -10,5 +10,5 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
 server.listen(port, function () {
-    console.log(`BACKEND is running on port ${port}.`)
+    console.log(`BACKEND is running on port ${port}.`);
 });
